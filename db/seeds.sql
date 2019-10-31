65b30609f776e2f9b1034520b6fcb3da334355ae
Charles Tuggle <tuggle.charles@gmail.com>
2019-10-31 19:06:57 -0400

    adding heroku deployment process including access to JAWSDB_URL

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Big Mac');
+INSERT INTO burgers (name) VALUES ('Whopper');
+INSERT INTO burgers (name, devoured) VALUES ('Smashburger', true);
+INSERT INTO burgers (name, devoured) VALUES ('White Castle Slider', true);
+INSERT INTO burgers (name, devoured) VALUES ('Chicken Sandwich', true);
+INSERT INTO burgers (name) VALUES ('Patty Melt');
