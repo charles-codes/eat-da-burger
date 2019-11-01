eea39a6e96f0aacd3e4f668599a6652f2598b9a2
Charles Tuggle <tuggle.charles@gmail.com>
2019-10-31 23:43:29 -0400

    removed comments for cleaner code

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,11 +1,7 @@
 const express = require("express");
-
 const router = express.Router();
-
-// Import the model (burger.js) to use its database functions.
 var burger = require("../models/burger.js");
 
-// Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
   burger.selectAll(function(data) {
     var hbsObject = {
@@ -21,7 +17,6 @@ router.post("/api/burgers", function(req, res) {
     ["burger_name"],
     [req.body.burger_name],
     function(result) {
-      // Send back the ID of the new quote
       res.json({ id: result.insertId });
     }
   );
@@ -29,10 +24,6 @@ router.post("/api/burgers", function(req, res) {
 
 router.put("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
-  
-  // console.log("------------------------------------");
-  // console.log(req.body);
-  // console.log("------------------------------------");
   console.log("condition", condition);
 
   burger.updateOne(
@@ -40,7 +31,6 @@ router.put("/api/burgers/:id", function(req, res) {
     condition,
     function(result) {
       if (result.changedRows == 0) {
-        // If no rows were changed, then the ID must not exist, so 404
         return res.status(404).end();
       } else {
         res.status(200).end();
--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,43 +1,35 @@
-// Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
   $(".eatButton").on("click", function(event) {
     var id = $(this).data("id");
-    // var isEat = $(this).data("devoured");
 
     var newEat = {
       devoured: true
     };
 
-    // Send the PUT request.
     $.ajax("/api/burgers/" + id, {
       type: "PUT",
       data: newEat
     }).then(
       function() {
         console.log("changed devoured to" + newEat);
-        // Reload the page to get the updated list
         location.reload();
       }
     );
   });
 
-  // Button to remake a burger, the opposite of eat a burger
   $(".makeButton").on("click", function(event) {
     var id = $(this).data("id");
-    // var isEat = $(this).data("devoured");
 
     var newEat = {
       devoured: false
     };
 
-    // Send the PUT request.
     $.ajax("/api/burgers/" + id, {
       type: "PUT",
       data: newEat
     }).then(
       function() {
         console.log("changed devoured  to", newEat);
-        // Reload the page to get the updated list
         location.reload();
       }
     );
@@ -49,21 +41,18 @@ $(function() {
   
   
   $(".create-form").on("submit", function(event) {
-    // Make sure to preventDefault on a submit event.
     event.preventDefault();
 
     let newBurger = {
       burger_name: $("#burg").val().trim()
     };
 
-    // Send the POST request.
     $.ajax("/api/burgers", {
       type: "POST",
       data: newBurger
     }).then(
       function() {
         console.log("created new burger");
-        // Reload the page to get the updated list
         location.reload();
       }
     );
@@ -72,13 +61,11 @@ $(function() {
   $(".delete-cat").on("click", function(event) {
     var id = $(this).data("id");
 
-    // Send the DELETE request.
     $.ajax("/api/cats/" + id, {
       type: "DELETE"
     }).then(
       function() {
         console.log("deleted cat", id);
-        // Reload the page to get the updated list
         location.reload();
       }
     );
